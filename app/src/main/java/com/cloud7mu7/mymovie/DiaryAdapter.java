f7d58d68655fe144e9a656d0bd3c0a491a11463f
cloudmu7 <cloudmu7777@naver.com>
2021-11-05 16:39:42 +0900

    커스텀 어뎁터 구현

--- app/src/main/java/com/cloud7mu7/mymovie/DiaryAdapter.java
+++ app/src/main/java/com/cloud7mu7/mymovie/DiaryAdapter.java
@@ -1,27 +1,69 @@
 package com.cloud7mu7.mymovie;
 
+import android.content.ClipData;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class DiaryAdapter extends RecyclerView.Adapter {
+
+    Context context;
+    ArrayList<Item> items;
+
+    public DiaryAdapter(Context context, ArrayList<Item> items){
+        this.context = context;
+        this.items = items;
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemview = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_diary, parent, false);
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View itemview = inflater.inflate(R.layout.activity_diary, parent, false);
+        VH vh = new VH(itemview);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        VH vh = (VH) holder;
+
+        Item item = items.get(position);
+
+        vh.tvTitle.setText(item.title);
+        vh.tvday.setText(item.day);
+        vh.tvMoviename.setText(item.moviename);
+        vh.tvcoment.setText(item.coment);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
+    }
+
+    class VH extends RecyclerView.ViewHolder{
+
+        TextView tvTitle;
+        TextView tvMoviename;
+        TextView tvday;
+        TextView tvcoment;
+
+        public VH(@NonNull View itemView) {
+            super(itemView);
+
+            tvTitle = itemView.findViewById(R.id.diary_title);
+            tvMoviename = itemView.findViewById(R.id.diary_moviename);
+            tvday = itemView.findViewById(R.id.diary_day);
+            tvcoment = itemView.findViewById(R.id.diary_coment);
+
+        }
     }
 }
---
+++ app/src/main/java/com/cloud7mu7/mymovie/Item.java
@@ -0,0 +1,21 @@
+package com.cloud7mu7.mymovie;
+
+public class Item {
+
+    int no;
+    String title;
+    String day;
+    String moviename;
+    String coment;
+
+    public Item(){
+    }
+
+    public Item(int no, String title, String moviename, String coment){
+        this.no = no;
+        this.title = title;
+        this.day = day;
+        this.moviename = moviename;
+        this.coment = coment;
+    }
+}
