0caaf867d0bbaf9610bf7dfcb221e7f733f9face
cloudmu7 <cloudmu7777@naver.com>
2021-11-10 17:39:32 +0900

    다이어리 구현

--- app/src/main/java/com/cloud7mu7/mymovie/DiaryActivity.java
+++ app/src/main/java/com/cloud7mu7/mymovie/DiaryActivity.java
@@ -1,17 +1,39 @@
 package com.cloud7mu7.mymovie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class DiaryActivity extends AppCompatActivity {
+
+    ArrayList<Item> items = new ArrayList<>();
+
+    RecyclerView recyclerView;
+    DiaryAdapter diaryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+
+        recyclerView = findViewById(R.id.diary_recycler);
+        diaryAdapter = new DiaryAdapter(this, items);
+        recyclerView.setAdapter(diaryAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    void loadDB(){
+        items.clear();
+        diaryAdapter.notifyDataSetChanged();
     }
 
     public void clickDiaryEdit(View view) {
